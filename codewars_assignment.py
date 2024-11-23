4475c86e4e9175952b582ba898f1fbbae0b32850
Adela Olivera <adela.olivera@yahoo.com>
2024-11-23 15:14:31 -0600

    First Commit

---
+++ codewars_assignment.py
@@ -0,0 +1,28 @@
+#   Assignment: Embarking on Your Codewars Journey
+
+#   Even or Odd
+
+def even_or_odd(number):
+    pass
+    
+    if (number % 2) == 0:
+        return "Even"
+    else:
+        return "Odd" 
+    
+#   Convert a Number to a String
+
+def number_to_string(num):
+    # Return a string of the number here!
+    num2 = str(num)
+    return num2
+
+#   Vowel Count
+
+def get_count(sentence):
+    pass
+    count = 0
+    for x in sentence:
+        if x == "a" or x == "e" or x == "i" or x == "o" or x == "u":
+            count = count + 1
+    return count
